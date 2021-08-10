2662325de1d12d7eff966ab5f787274f665b35d5
Mat Jones <mat.jones@agilebits.com>
2021-08-10 15:06:03 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# yaclt.nvim - WIP
+
+A Neovim plugin for [Yaclt](https://github.com/mrjones2014/yaclt).
+
+## Installation
+
+### Packer
+
+`use mrjones2014/yaclt.nvim`
+
+### Vim Plug
+
+`Plug mrjones2014/yaclt.nvim`
---
+++ lua/yaclt/init.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+M.config = {
+  cmd = 'yaclt',
+}
+
+function M.setup(config)
+  if config != nil and config.cmd != nil then
+    M.config.cmd = config.cmd
+  end
+end
+
+function M.yacltNew(args)
+  local commandString = M.config.cmd
+  if args != nil then
+    for arg in args do
+      commandString += ' ' .. arg
+    end
+  end
+  local process = io.popen(M.config.cmd)
+  local stdoutLines = process:lines()
+  assert(process.close() == 0) -- assert command executed successfully
+  local filepath = stdoutLines[0]
+  vim.api.nvim_command('edit ' .. filepath)
+end
+
+return M
